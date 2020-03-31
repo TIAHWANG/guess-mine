7347b0da605c5a508e10c194499e93c55ebb0a3d
Tia Hwang <tiajshwang@gmail.com>
2020-03-31 14:31:15 +0900

    Server Setup-2

--- src/server.js
+++ src/server.js
@@ -1,1 +1,16 @@
+import { join } from "path";
 import express from "express";
+import socketIO from "socket.io";
+
+const PORT = 3000;
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", join(__dirname, "views"));
+app.use(express.static(join(__dirname, "static")));
+
+app.get("/", (req, res) => res.render("home"));
+
+const handleListen = () => console.log(`💜 Server http://localhost:${PORT}`);
+
+app.listen(PORT, handleListen);
