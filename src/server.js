8685fd7b22501c7c16c0381f8734ddf65fec813a
Tia Hwang <tiajshwang@gmail.com>
2020-04-02 16:20:21 +0900

    Socket.io

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
+/* eslint-disable prettier/prettier */
 import { join } from "path";
 import express from "express";
+import logger from "morgan";
 import socketIO from "socket.io";
 
 const PORT = 3000;
@@ -7,6 +9,7 @@ const app = express();
 
 app.set("view engine", "pug");
 app.set("views", join(__dirname, "views"));
+app.use(logger("dev"));
 app.use(express.static(join(__dirname, "static")));
 
 app.get("/", (req, res) => res.render("home"));
@@ -15,4 +18,18 @@ const handleListen = () => console.log(`💜 Server http://localhost:${PORT}`);
 
 const server = app.listen(PORT, handleListen);
 
-const io = socketIO(server);
+const io = socketIO.listen(server);
+
+let sockets = [];
+
+io.on("connection", socket => {
+    socket.on("newMessage", ({ message }) => {
+        socket.broadcast.emit("messageNotif", {
+            message,
+            nickname: socket.nickname || "Anon"
+        });
+    });
+    socket.on("setNickname", ({ nickname }) => {
+        socket.nickname = nickname;
+    });
+});
--- src/static/index.js
+++ src/static/index.js
@@ -1,1 +1,20 @@
-hello;
+// eslint-disable-next-line no-undef
+const socket = io("/");
+
+socket.on("Hello", () => console.log("Somebody joined"));
+
+function sendMessage(message) {
+    socket.emit("newMessage", { message });
+    console.log(`You: ${message}`);
+}
+
+function setNickname(nickname) {
+    socket.emit("setNickname", { nickname });
+}
+
+function handleMessageNotif(data) {
+    const { message, nickname } = data;
+    console.log(`${nickname}: ${message}`);
+}
+
+socket.on("messageNotif", handleMessageNotif);
